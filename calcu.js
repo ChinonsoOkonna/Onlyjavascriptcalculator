8796d63ead0c53c4156357351590a69787517225
ChinonsoOkonna <favourchinonso07@gmail.com>
2022-06-27 17:19:26 +0100

    first commit

---
+++ calcu.js
@@ -0,0 +1,34 @@
+var x = prompt("Type a Number");
+var z = prompt("Enter Operator \n 1.Add \n 2.Substrate \n 3.Multiply \n 4.Divide");
+var y = prompt("Type a second Number");
+
+if(z == 1) {
+    var Result = Number(x) + Number(y);
+    alert(Result);
+    console.log(Result);
+}
+
+if(z == 2) {
+    var Result = Number(x) - Number(y);
+    alert(Result);
+    console.log(Result);
+}
+
+if(z == 3) {
+    var Result = Number(x) * Number(y);
+    alert(Result);
+    console.log(Result);
+}
+
+if(z == 4) {
+    var Result = Number(x) / Number(y);
+    alert(Result);
+    console.log(Result);
+}
+
+var final = ("Hope you had a good user experience?")
+alert(final)
+
+
+
+ 
